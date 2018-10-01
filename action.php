98db28f8688ead4633e8bd6a8be780995345ab0d
mike-div51 <43147461+mike-div51@users.noreply.github.com>
2018-10-01 10:19:20 -0500

    php add but wont work on github

--- action.php
+++ action.php
@@ -5,16 +5,17 @@
 
 $name=$_POST["name"];
 $email=$_POST["email"];
-$category=$_POST["category"];
-$priority=$_POST["priority"];
-$copy=$_POST["copy"];
+// $category=$_POST["category"];
+// $priority=$_POST["priority"];
+// $copy=$_POST["copy"];
 $message=$_POST["message"];
 
-
-
-$message="Email: $email \nName: $name \nCategory: $category \nPriority: $priority \nMessage: $message";
+$message="Email: $email \nName: $name \nMessage: $message";
+// $message="Email: $email \nName: $name \nCategory: $category \nPriority: $priority \nMessage: $message";
 
 mail( "mike@div51.com", "Email from Website", "$message");
-header( "Location: index.html");
+header( "Location: thanks.html");
+
 exit;
+
 ?>
